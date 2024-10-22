194c3ca885d054708b8cc2a44da68204d95fb245
zevaguillo <guiller.zeva16@gmail.com>
2024-10-21 23:19:26 -0500

    feat: se agrego proyecto inicial de spring boot

---
+++ backend/src/main/java/com/zevaguillo/virtualToken/VirtualTokenApplication.java
@@ -0,0 +1,13 @@
+package com.zevaguillo.virtualToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualTokenApplication.class, args);
+	}
+
+}
